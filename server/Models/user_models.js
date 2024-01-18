bf6fcf36e56dd79d59a7b390ca4e476b66080860
JoeA <joealexanderadams@gmail.com>
2024-01-18 13:57:44 +0000

    complete task 17

--- server/Models/user_models.js
+++ server/Models/user_models.js
@@ -5,3 +5,13 @@ exports.fetchUsers = () => {
     SELECT * FROM users`)
     .then(({rows}) => rows)
 }
+
+exports.fetchUsersByUsername = (username) => {
+    return db.query(`
+    SELECT username, avatar_url, name FROM users
+    WHERE username = $1`,[username])
+    .then(({rows}) => {
+        if (rows.length === 0) return Promise.reject({status:404, msg:"Not Found"})
+        return rows
+    })
+}
