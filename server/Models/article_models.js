22691ce782778b5c95b5b02468aea1f7d7fe0819
JoeA <joealexanderadams@gmail.com>
2024-01-25 17:03:24 +0000

    fix offset

--- server/Models/article_models.js
+++ server/Models/article_models.js
@@ -21,7 +21,7 @@ exports.fetchArticles = ({topic, sort_by="created_at",order="desc",limit = 10, p
     query += `    
     GROUP BY articles.article_id
     ORDER BY articles.${sort_by} ${order}
-    LIMIT ${limit} OFFSET ${(p -1) * 5}`;
+    LIMIT ${limit} OFFSET ${(p -1) * limit}`;
 
     return db.query(query,args).then(({rows}) => rows)
 }
