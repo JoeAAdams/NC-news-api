74904e485ce5e9a1a1a7553da4ee0b9448d9de7c
JoeA <joealexanderadams@gmail.com>
2024-02-05 09:49:10 +0000

    add cors

--- server/app.js
+++ server/app.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const apiRouter = require('./routes/api');
+const express = require("express");
+const apiRouter = require("./routes/api");
 const app = express();
+const cors = require("cors");
 
+app.use(cors())
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api', apiRouter)
+app.use("/api", apiRouter);
 
-
-app.use((err,req,res,next)=>{
-    if (err.msg && err.status){
-        res.status(err.status).send(err)
+app.use((err, req, res, next) => {
+    if (err.msg && err.status) {
+        res.status(err.status).send(err);
     }
-    next(err)
-})
+    next(err);
+});
 
-app.use((err,req,res,next) => {
-    if(err.code === '22P02' || '23502') res.status(400).send({status:400, msg: "Bad Request"})
-})
+app.use((err, req, res, next) => {
+    if (err.code === "22P02" || "23502")
+        res.status(400).send({ status: 400, msg: "Bad Request" });
+});
 module.exports = app;
